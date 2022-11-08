9704537e4b80c8d71432b2cfdf691b732e9044a2
VitoMAC <yohanes.vito@ti.ukdw.ac.id>
2022-11-08 12:12:34 +0700

    add fetch api

---
+++ cekcuaca/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <input type="text" v-model="message" placeholder="masukkan pesan"/>
+  <button @click="kirim()">Kirim Pesan </button>
+  <pre> Response: {{info}}</pre>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      message: "",
+      info: ""
+    }
+  },
+  methods: {
+    kirim(){
+      this.axios.get("https://indodax.com/api/"+this.message+"_idr/ticker")
+      .then(response => {
+        var harga = response.data.ticker.last;
+        this.info = "harga terakhir " + harga
+  })},
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
